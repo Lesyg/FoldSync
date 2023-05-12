bd86b3e0dbb9c4226d6cf6ac0d7402c663b31d7e
Dominik Gysel <gyseldom@fit.cvut.cz>
2023-05-12 12:35:30 +0200

    Added logging capabilities

--- main.py
+++ main.py
@@ -15,62 +15,41 @@
 # It is allowed (and recommended) to use external libraries implementing other well-known algorithms. For example, there is no point in implementing yet another function that calculates MD5 if you need it for the task – it is perfectly acceptable to use a  third-party (or built-in) library.
 
 
-# handle cli arguments
-
-# for each item in source folder 
 
 import click
 from pathlib import Path
 import shutil
 import os
+import logging
 
 
-# @click.option('-l', '--log', help="Path to the log file")
+# parser.add_argument('-r', '--repeat', help="repeat interval", required=False, type=int)
+
 @click.command()
 @click.argument('source_dir', type=click.Path(path_type=Path))
 @click.argument('replica_dir', type=click.Path(path_type=Path))
-def run_sync(source_dir: Path, replica_dir: Path) -> None:
+@click.option('-l', '--log', type=click.Path(path_type=Path), help='Path to the log file')
+def run_sync(source_dir: Path, replica_dir: Path, log: Path) -> None:
     """
     Synchronize replica_dir with source_dir\n
     :param source_dir: Path to the source directory\n
     :param replica_dir: Path to the destination directory
+    :param log: Path to the log file
     """
-    if not source_dir.exists() or not replica_dir.exists():
-        click.echo("The target directory doesn't exist")
-        raise SystemExit(1)
 
-    click.echo('SOURCE DIRECTORY:')
+    setup_log(log)
 
-    simple_sync(source_dir, replica_dir)
+    logging.info(f"Starting synchronization program")
 
-    # copy_files(source_dir, replica_dir)
+    logging.info("Validating directories")
+    validate(source_dir, replica_dir)
 
-    print('\nREPLICA DIRECTORY:')
+    logging.info("Starting synchronization")
 
-    for entry in replica_dir.iterdir():
-        click.echo(f'{entry.name}, size: {entry.stat().st_size}')
+    copy_files(source_dir, replica_dir)
 
-    # parser = argparse.ArgumentParser(
-    #     prog='FoldSync',
-    #     description='FoldSync synchronizes the replica folder with source folder',
-    #     epilog='Text at the bottom of help' # TODO improve this message
-    # )
+    logging.info("Synchronization succesfull, ending program\n")
 
-    # parser.add_argument("source_folder", help="path to the source folder")
-    # parser.add_argument("replica_folder", help="path to the replica folder")
-    # parser.add_argument('-r', '--repeat', help="repeat interval", required=False, type=int)
-    # parser.add_argument('-l', '--log', help="path to log file", required=False)
-
-def simple_sync(source: Path, destination: Path) -> None:
-    """
-    Very simple synchronization function
-    First it recursively clears the folder, and then it recursively copies the folder
-    :param source: Path to the source folder
-    :param destination: Path to the destination folder
-    :return:
-    """
-    shutil.rmtree(destination)
-    shutil.copytree(source, destination)
 
 def copy_files(source: Path, destination: Path) -> None:
     if not destination.exists():
@@ -79,8 +58,51 @@ def copy_files(source: Path, destination: Path) -> None:
         if file.is_dir():
             copy_files(file, destination.joinpath(file.name))
         else:
-            click.echo(f'Copying file {file.name} of size: {file.stat().st_size} from {source} directory to {destination} directory')
+            logging.info(
+                f'Copying file {file.name} of size: {file.stat().st_size} from {source.absolute()} directory to {destination.absolute()} directory')
             shutil.copy2(os.path.join(source, file.name), destination)
+
+
+def validate(source_dir: Path, replica_dir: Path) -> None:
+    """
+    Validates the source and target directory
+    If the target directory doesn't exist it creates it
+    :param source_dir:
+    :param replica_dir:
+    :return: None
+    """
+    if not os.access(source_dir, os.R_OK):
+        logging.error(f"The source directory {source_dir} is not accessible")
+        logging.error("Exiting application\n")
+        raise SystemExit(1)
+
+    if not source_dir.exists():
+        logging.error(f"The source directory {source_dir.absolute()} doesn't exist")
+        logging.error("Can't perform synchronization")
+        logging.error("Exiting application\n")
+        raise SystemExit(1)
+
+    if not replica_dir.exists():
+        logging.warning(f"The target directory {replica_dir.absolute()} doesn't exist")
+        logging.info(f"Creating target directory {replica_dir.absolute()}")
+        Path.mkdir(replica_dir)
+
+
+def setup_log(file: Path) -> None:
+    """
+    Configures logging
+    :param file: path to the file logs will be written to
+    :return: None
+    """
+    if file:
+        logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s', level=logging.INFO, handlers=[
+            logging.FileHandler(file),
+            logging.StreamHandler()
+        ])
+    else:
+        logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s', level=logging.INFO, handlers=[
+            logging.StreamHandler()
+        ])
 
 
 if __name__ == '__main__':
